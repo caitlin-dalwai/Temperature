62bff5d009f31b29ab7a4e64a165f0dd69d5c2a5
caitlin-dalwai <dalwaicaitlin@gmail.com>
2024-05-17 16:16:51 +0200

    bmi

---
+++ script/temp.js
@@ -0,0 +1,25 @@
+let celsius = document.getElementById('low')
+let fahren = document.getElementById('high')
+let convert = document.getElementById('calc')
+let clear = document.getElementById('clr')
+
+convert.addEventListener('click', (event)=>{
+    fahren.innerText += parseFloat(eval(`${celsius.value}*9/5+32`)).toFixed(2)
+})
+
+clear.addEventListener('click', function() {
+    clearSum();
+});
+
+function clearSum() {
+    
+fahren.innerText = 'Fahrenheit:';
+}
+
+
+
+
+
+
+
+
